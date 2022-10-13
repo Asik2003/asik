deac70be8b2063f53a222c177366dc080fc4edde
Assylkhan <you@example.com>
2022-10-13 12:16:05 +0600

    second commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
@@ -36,11 +37,20 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
 
     class Dice(val numSides: Int) {
 
         fun roll(): Int {
+
             return (1..numSides).random()
         }
 
