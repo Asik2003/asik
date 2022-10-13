6413c0917f2cd55285c12e83b03d9412b5b4954a
Assylkhan <you@example.com>
2022-10-13 11:52:44 +0600

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button;
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val diceImage: ImageView = findViewById(R.id.imageView)
+            rollDice()
+        }
+
+
+    }
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice__4_)
+            2 -> diceImage.setImageResource(R.drawable.dice__5_)
+            3 -> diceImage.setImageResource(R.drawable.dice__1_)
+            4 -> diceImage.setImageResource(R.drawable.dice)
+            5 -> diceImage.setImageResource(R.drawable.dice__2_)
+            6 -> diceImage.setImageResource(R.drawable.dice__3_)
+        }
+
+    }
+
+
+    class Dice(val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+
+   }
+
+}
