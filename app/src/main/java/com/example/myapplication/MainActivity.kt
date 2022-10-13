af2ec810823fb16cc6a09c094a47d43b50fd04ca
Assylkhan <you@example.com>
2022-10-13 12:17:18 +0600

    3 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -24,28 +24,11 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-
-        val diceImage: ImageView = findViewById(R.id.imageView)
-
-        when (diceRoll) {
-            1 -> diceImage.setImageResource(R.drawable.dice__4_)
-            2 -> diceImage.setImageResource(R.drawable.dice__5_)
-            3 -> diceImage.setImageResource(R.drawable.dice__1_)
-            4 -> diceImage.setImageResource(R.drawable.dice)
-            5 -> diceImage.setImageResource(R.drawable.dice__2_)
-            6 -> diceImage.setImageResource(R.drawable.dice__3_)
-        }
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
 
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
-        }
 
     class Dice(val numSides: Int) {
 
